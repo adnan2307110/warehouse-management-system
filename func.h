9fa471f0e96a8e1e03d4bfc369b41d5e72a08a52
Adnan <mdadnanh062@gmail.com>
2025-01-29 21:02:58 +0600

    Add files via upload
    main branch

---
+++ func.h
@@ -0,0 +1,21 @@
+//function.h
+#ifndef FUCNTION_H
+#define FUNCTION_H
+//function prototypes
+void addItem();
+void editItem();
+void deleteItem();
+void displayStock();
+void searchItem();
+void updateStock();
+void recordSale();
+void generateReport();
+void addEmployeeinfo();
+void editEmployeeinfo();
+void deleteEmployee();
+void displayEmployeesinfo();
+void saveToFile();
+void getfromfile();
+void menudesign();
+
+#endif
